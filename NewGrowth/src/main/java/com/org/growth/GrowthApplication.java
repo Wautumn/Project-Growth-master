32c81c263f2570f7f1437be1051456722dafcfb7
WHQ <2283149823@qq.com>
2018-11-19 21:36:12 +0800

    init

---
+++ NewGrowth/src/main/java/com/org/growth/GrowthApplication.java
@@ -0,0 +1,12 @@
+package com.org.growth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowthApplication.class, args);
+    }
+}
