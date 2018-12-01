07eedac695b6e1af9e4a562c29d512a4d1eb5ebb
WHQ <2283149823@qq.com>
2018-12-01 14:08:12 +0800

    Add RespMessage Bean

--- src/main/java/com/org/growth/controller/ShareTomatoController.java
+++ src/main/java/com/org/growth/controller/ShareTomatoController.java
@@ -1,14 +1,12 @@
 package com.org.growth.controller;
-
 import com.org.growth.Service.UserService;
+import com.org.growth.entity.RespBean;
 import com.org.growth.entity.User;
 import com.org.growth.utils.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/")
@@ -18,19 +16,18 @@ public class ShareTomatoController {
     UserService userService=new UserService();
 
     @RequestMapping(value = "/share")
-    public int shareTomato(){
-        User user= Util.getCurrentUser();
-        Long userId=user.getId();
-        int count=userService.getTomatoWeeklyCount(userId);
-
-        if(count<1){
+    public RespBean shareTomato() {
+        User user = Util.getCurrentUser();
+        Long userId = user.getId();
+        int count = userService.getTomatoWeeklyCount(userId);
+        if (count < 1) {
             //无法分享
-        }else{
+            return new RespBean("error", "没有足够的番茄")
+        } else {
             userService.TomatoCountReduceOne(userId);//数量减一
             //唤起分享接口
-
+            return new RespBean("success", "分享成功！");
         }
-
     }
 
 }
