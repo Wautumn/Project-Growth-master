4591c8a48ea54286cfb6f68e183bf30743e20e36
WHQ <2283149823@qq.com>
2018-12-04 20:26:00 +0800

    Add JSoup

---
+++ src/main/java/com/org/growth/utils/getArticleByWeb.java
@@ -0,0 +1,21 @@
+package com.org.growth.utils;
+
+import org.jsoup.Jsoup;
+
+import javax.swing.text.Document;
+
+public class getArticleByWeb {
+    public void get() {
+        new Thread(new Runnable() {
+            public void run() {
+                try{
+                 //   Document doc= Jsoup.connect()
+                }catch(Exception e){
+
+                }
+
+            }
+
+        }).start();
+    }
+}
