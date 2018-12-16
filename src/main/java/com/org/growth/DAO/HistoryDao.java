43c40ac3eaa0b47a59fd2fad905363701f4421fe
ruaruaruabick <993492964@qq.com>
2018-12-17 00:26:19 +0800

    daily update

--- src/main/java/com/org/growth/DAO/HistoryDao.java
+++ src/main/java/com/org/growth/DAO/HistoryDao.java
@@ -3,6 +3,7 @@ package com.org.growth.DAO;
 import com.org.growth.entity.History;
 import org.springframework.data.domain.Page;
 
+import java.util.Date;
 import java.util.List;
 
 public interface HistoryDao {
@@ -10,12 +11,12 @@ public interface HistoryDao {
     java.util.List<History> viewMonthHistory(long userId, String year);
     //Page<History> viewHistoryStatus(long userId, int size, int page, int status);
 
-    boolean saveStartTomato(long userId);
+    boolean saveStartTomato(long userId, Date startTime);
 
-    boolean saveBreakTomato(long userId);
+    boolean saveBreakTomato(long userId,Date startTime,Date endTime);
 
-    boolean saveEndTomato(long userId, boolean needAssociation);
+    boolean saveEndTomato(long userId, boolean needAssociation,Date startTime,Date endTime);
 
-    boolean saveEndTomato(long userId, boolean needAssociation, String taskName);
+    boolean saveEndTomato(long userId, boolean needAssociation, String taskName,Date startTime,Date endTime);
 
 }
